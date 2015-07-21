5695aa2f76e4b693ec944466c5ed66b8c7fff97f
Petr Damborsky <petr.damborsky@gmail.com>
2015-07-21 22:16:32 +0200

    reformatting in VS Code

--- index.js
+++ index.js
@@ -9,41 +9,41 @@ var files = fs.readdirSync('data');
 // First I need to read all the files.
 // Using async module to map every filename to it's content.
 async.map(
-	// 1st parameter is the array of filenames.
+// 1st parameter is the array of filenames.
 	files,
 	// 2nd parameter is the function that gets called on every filename
 	// Here I need to read the actual file (append the data folder) and
 	// wait for the result. Once the file is read, I'll just run the
 	// provided callback function which will add the file to internal
 	// files collection (I guess).
-	function(name,callback){
-		fs.readFile('data/'+name, function(err,data){
+	function (name, callback) {
+		fs.readFile('data/' + name, function (err, data) {
 			// Enable this console output if you want to look like a pro hacker.
 			// console.log(data.toString());
-			callback(null,data);
+			callback(null, data);
 		});
 	},
 	// 3rd parameter is the function that gets called once all the
 	// files are loaded. This is the place where I can start working with data.
-	function(err,files){
+	function (err, files) {
 		if (err)
 			throw err;
-			
+
 		var summary = {};
 		var logFormat = 'Waiting %d seconds for %s';
-			
-		files.forEach(function(f,i){
+
+		files.forEach(function (f, i) {
 			var har = JSON.parse(f);
 			var entries = har.log.entries;
 			
 			// weak/simple way of filtering out the xhrs
-			var xhrs = _.filter(entries, {response: {content:{mimeType:'application/json'}}});
+			var xhrs = _.filter(entries, { response: { content: { mimeType: 'application/json' } } });
 			
 			// console.log(util.format('%d. HAR',i));
-			_.forEach(_.sortByOrder(xhrs,'timings.wait','desc'), function(item){
+			_.forEach(_.sortByOrder(xhrs, 'timings.wait', 'desc'), function (item) {
 				// saving to summary
 				var newValue = Math.floor(parseFloat(item.timings.wait));
-				summary[item.request.url] = summary[item.request.url] ? parseInt(summary[item.request.url],10) : 0;
+				summary[item.request.url] = summary[item.request.url] ? parseInt(summary[item.request.url], 10) : 0;
 				summary[item.request.url] += newValue;
 				// debug output
 				// console.log(util.format(logFormat, (item.timings.wait/1000).toPrecision(2), item.request.url));
@@ -54,9 +54,9 @@ async.map(
 		console.log('Summary of average waiting times:');
 		
 		// TODO: orderby
-		for (prop in summary){
-			var avg = summary[prop]/Object.getOwnPropertyNames(summary).length
-			console.log(util.format(logFormat, (avg/1000).toPrecision(3), prop));
+		for (prop in summary) {
+			var avg = summary[prop] / Object.getOwnPropertyNames(summary).length
+			console.log(util.format(logFormat, (avg / 1000).toPrecision(3), prop));
 		}
 	}
-);
+	);
