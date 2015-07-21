8ad72f4df045b5b7b0eafd36572f0f87e8d4ac59
Petr Damborsky <pdamb@email.cz>
2015-07-21 20:01:36 +0200

    Adding basic logic for getting the average times.

---
+++ README.md
@@ -0,0 +1,9 @@
+#HARRIS: The .har analyzer
+
+##Use case 1:
+Grab multiple hars with similar xhrs and output their average length.
+
+
+###Installation
+- npm install
+- node index.js
--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 var async = require('async');
 var fs = require('fs');
+var util = require('util');
+var _ = require('lodash');
 
 // save HARs in data folder (e.g. "data/report1.json")
 var files = fs.readdirSync('data');
-
-// read the files
-// files.forEach(function(filename){
-// 	console.log(filename);
-// });
 
 // First I need to read all the files.
 // Using async module to map every filename to it's content.
@@ -31,9 +28,35 @@ async.map(
 	function(err,files){
 		if (err)
 			throw err;
-		files.forEach(function(f){
-			console.log(f);
-	});
-});
-
-console.log('done');
+			
+		var summary = {};
+		var logFormat = 'Waiting %d seconds for %s';
+			
+		files.forEach(function(f,i){
+			var har = JSON.parse(f);
+			var entries = har.log.entries;
+			
+			// weak/simple way of filtering out the xhrs
+			var xhrs = _.filter(entries, {response: {content:{mimeType:'application/json'}}});
+			
+			// console.log(util.format('%d. HAR',i));
+			_.forEach(_.sortByOrder(xhrs,'timings.wait','desc'), function(item){
+				// saving to summary
+				var newValue = Math.floor(parseFloat(item.timings.wait));
+				summary[item.request.url] = summary[item.request.url] ? parseInt(summary[item.request.url],10) : 0;
+				summary[item.request.url] += newValue;
+				// debug output
+				// console.log(util.format(logFormat, (item.timings.wait/1000).toPrecision(2), item.request.url));
+			});
+		});
+		
+		// process summary
+		console.log('Summary of average waiting times:');
+		
+		// TODO: orderby
+		for (prop in summary){
+			var avg = summary[prop]/Object.getOwnPropertyNames(summary).length
+			console.log(util.format(logFormat, (avg/1000).toPrecision(3), prop));
+		}
+	}
+);
